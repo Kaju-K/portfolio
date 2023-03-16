719fc5749cb061cf6eeb1b2bd7ed7f8695b23a47
Kaju-K <marcelokogut3@gmail.com>
2023-03-16 11:29:16 +0200

    adding shakira page and making menu close when section is chosen in phones

--- app/menuButton.js
+++ app/menuButton.js
@@ -1,8 +1,20 @@
 menuButton = document.querySelector(".menu-button")
 navigationBar = document.querySelector(".navigation-bar")
 menuImage = document.getElementById("menu-image")
+navigationLinks = document.querySelectorAll(".navigation-bar__links")
+
+const windowWidth = window.innerWidth
 
 menuButton.addEventListener("click", () => {
     ["fa-bars", "fa-xmark"].map( img => menuImage.classList.toggle(img))
     navigationBar.classList.toggle("navigation-bar--active")
 } )
+
+if (windowWidth < 900) {
+    navigationLinks.forEach(link => {
+        link.addEventListener("click", event => {
+            ["fa-bars", "fa-xmark"].map( img => menuImage.classList.toggle(img))
+            navigationBar.classList.toggle("navigation-bar--active")
+        })
+    });
+}
