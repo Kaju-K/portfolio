4430b5661e2dcf583633ec8bcd28fdb7b18ad277
Kogut <marcelokogut3@gmail.com>
2024-09-17 13:12:02 +0300

    adjusting portfolio

--- app/skillsButton.js
+++ app/skillsButton.js
@@ -1,28 +1,32 @@
-allLogos = document.querySelectorAll(".skills-logos")
-explanationTitle = document.querySelector(".skills-explanation-title")
-explanation = document.querySelector(".skills-explanation")
+allLogos = document.querySelectorAll(".skills-logos");
+explanationTitle = document.querySelector(".skills-explanation-title");
+explanation = document.querySelector(".skills-explanation");
 
-defaultMessage = "Put your mouse over one of the skills to know a little bit more about it."
+defaultMessage = "Put your mouse over one of the skills to know a little bit more about it.";
 
 descriptions = {
-    "html": "HTML is the standard markup language for documents designed to be displayed in a web browser.",
-    "css": "CSS is a style sheet language used for describing the presentation of a document written in a markup language",
-    "js": "JS is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
-    "python": "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
-    "react": "React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.",
-    "git": "GIT is a distributed version control system: tracking changes in any set of files, usually used for coordinating work among programmers collaboratively.",
-    "github": "Github is an Internet hosting service for software development and version control using Git."
-}
+  html: "HTML is the standard markup language for documents designed to be displayed in a web browser.",
+  css: "CSS is a style sheet language used for describing the presentation of a document written in a markup language",
+  js: "JS is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+  python:
+    "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
+  react: "React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.",
+  elixir: "Elixir is a dynamic, functional language for building scalable and maintainable applications.",
+  mongodb:
+    "MongoDB is a source-available, cross-platform, document-oriented database program. Classified as a NoSQL database product, MongoDB utilizes JSON-like documents with optional schemas.",
+  git: "GIT is a distributed version control system: tracking changes in any set of files, usually used for coordinating work among programmers collaboratively.",
+  github: "Github is an Internet hosting service for software development and version control using Git."
+};
 
-allLogos.forEach( (logo) => {
-    logo.addEventListener( "mouseover", (e) => {
-        logoName = e.target.dataset["logo"];
-        explanationTitle.innerHTML = `${logoName.toUpperCase()}`;
-        explanation.innerHTML = descriptions[logoName];
-    });
+allLogos.forEach((logo) => {
+  logo.addEventListener("mouseover", (e) => {
+    logoName = e.target.dataset["logo"];
+    explanationTitle.innerHTML = `${logoName.toUpperCase()}`;
+    explanation.innerHTML = descriptions[logoName];
+  });
 
-    logo.addEventListener( "mouseout", (e) => {
-        explanationTitle.innerHTML = "";
-        explanation.innerHTML = defaultMessage;
-    });
+  logo.addEventListener("mouseout", (e) => {
+    explanationTitle.innerHTML = "";
+    explanation.innerHTML = defaultMessage;
+  });
 });
--- app/skillsButtonWheel.js
+++ app/skillsButtonWheel.js
@@ -1,30 +1,37 @@
-orbitLogos = document.querySelectorAll(".orbit-image")
-centerImage = document.querySelector(".container-center-image")
-centerText = document.querySelector(".center-text")
+orbitLogos = document.querySelectorAll(".orbit-image");
+centerImage = document.querySelector(".container-center-image");
+centerText = document.querySelector(".center-text");
 
-defaultMessage = "Click in one of the skills to know a little bit more about it."
+defaultMessage = "Click in one of the skills to know a little bit more about it.";
 
 descriptions = {
-    "html": "HTML is the standard markup language for documents designed to be displayed in a web browser.",
-    "css": "CSS is a style sheet language used for describing the presentation of a document written in a markup language",
-    "js": "JS is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
-    "python": "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
-    "react": "React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.",
-    "git": "GIT is a distributed version control system: tracking changes in any set of files, usually used for coordinating work among programmers collaboratively.",
-    "github": "Github is an Internet hosting service for software development and version control using Git."
-}
+  html: "HTML is the standard markup language for documents designed to be displayed in a web browser.",
+  css: "CSS is a style sheet language used for describing the presentation of a document written in a markup language",
+  js: "JS is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+  python:
+    "Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.",
+  react: "React is a free and open-source front-end JavaScript library for building user interfaces based on UI components.",
+  elixir: "Elixir is a dynamic, functional language for building scalable and maintainable applications.",
+  mongodb:
+    "MongoDB is a source-available, cross-platform, document-oriented database program. Classified as a NoSQL database product, MongoDB utilizes JSON-like documents with optional schemas.",
+  git: "GIT is a distributed version control system: tracking changes in any set of files, usually used for coordinating work among programmers collaboratively.",
+  github: "Github is an Internet hosting service for software development and version control using Git."
+};
 
-orbitLogos.forEach( logo => {
-    logo.addEventListener( "click", e => {
-        logoName = e.target.dataset["logo"]
-        if (logoName === "github") {
-            centerImage.innerHTML = `<a href="https://github.com/Kaju-K" target="_blank"><img src="img/logo/${logoName}-logo.svg"` +  ` alt=${logoName} class="center-image"></a>`
-        } else {
-            centerImage.innerHTML = `<img src="img/logo/${logoName}-logo.svg"` +  ` alt=${logoName} class="center-image">`
-        }
-        centerText.innerHTML = descriptions[logoName]
-
-    })
+orbitLogos.forEach((logo) => {
+  logo.addEventListener("click", (e) => {
+    logoName = e.target.dataset["logo"];
+    if (logoName === "github") {
+      centerImage.innerHTML =
+        `<a href="https://github.com/Kaju-K" target="_blank"><img src="img/logo/${logoName}-logo.svg"` +
+        ` alt=${logoName} class="center-image"></a>`;
+    } else {
+      centerImage.innerHTML = `<img src="img/logo/${logoName}-logo.svg"` + ` alt=${logoName} class="center-image">`;
+    }
+    centerText.innerHTML = descriptions[logoName];
+  });
 });
 
-centerImage.addEventListener("click", e => {console.log(e)})
+centerImage.addEventListener("click", (e) => {
+  console.log(e);
+});
